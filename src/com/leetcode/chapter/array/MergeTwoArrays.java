89f12c12fa0b9685ed8a2ffceeac26eba6ff33ce
Sarp Koc <s.sarp.koc@gmail.com>
2020-10-27 18:44:19 +0200

    added solution Merge two arrays

---
+++ src/com/leetcode/chapter/array/MergeTwoArrays.java
@@ -0,0 +1,35 @@
+package com.leetcode.chapter.array;
+
+public class MergeTwoArrays {
+
+	public static void main(String[] args) {
+		int[] input1 = {1,2,3,0,0,0};
+		int[] input2 = {2,5,6};
+		MergeTwoArrays merge = new MergeTwoArrays();
+		merge.merge(input1, 3, input2, 3);
+		System.out.println(input1);
+
+	}
+
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int sizeNums1 = m - 1;
+		int sizeNums2 = n - 1;
+		int size = nums1.length - 1;
+		for (int i = 0; i <= size; i++) {
+			if (sizeNums1 == -1) {
+				nums1[size - i] = nums2[sizeNums2];
+				sizeNums2--;
+			} else if (sizeNums2 == -1) {
+				nums1[size - i] = nums1[sizeNums1];
+				sizeNums1--;
+			} else if (nums1[sizeNums1] <= nums2[sizeNums2]) {
+				nums1[size- i] = nums2[sizeNums2];
+				sizeNums2--;
+			} else {
+				nums1[size - i] = nums1[sizeNums1];
+				sizeNums1--;
+			}
+		}
+	}
+
+}
